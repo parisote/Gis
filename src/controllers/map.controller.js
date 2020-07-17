9a8f74fce6c9dc35291c9126987ac515d624b791
parisote <detomas@gmail.com>
2020-07-17 18:40:20 -0300

    [delete test]

--- src/controllers/map.controller.js
+++ src/controllers/map.controller.js
@@ -130,15 +130,6 @@ mapCtrl.renderMap = async (req,res) => {
       n_json += ']}';
     }
 
-    const d = new PatientDiagnosis({
-      persoin_id: 12,
-      name: "asd",
-      diagnosis: "b",
-      geo: [1,1]
-    });
-
-    await d.save();
-
     if(req.cookies.diagnosis)
     {
       const dia = await PatientDiagnosis.find();
